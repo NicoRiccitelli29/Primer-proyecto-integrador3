6d0b934b2359707028e8f01d13fcb8265a90429f
NicoRiccitelli29 <nriccitelli@udesa.edu.ar>
2021-03-15 17:17:24 -0300

    Puse header en app.js

--- primer-proyecto/src/App.js
+++ primer-proyecto/src/App.js
@@ -1,6 +1,18 @@
 function App() {
   return (
     <div className="App">
+    <header class="uk-background-primary">
+      <section class=" uk-container uk-container-large">
+        <div class="">
+            <nav class="" uk-navbar>
+                <div class="uk-navbar-left">
+                    <h2 class="logo"><a href="">DNT App</a></h2>
+                </div>
+              </nav>  
+        </div>
+      </section> 
+    </header> 
+
       <footer class="uk-position-bottom uk-position-fixed uk-background-primary uk-flex uk-flex-between uk-flex-middle">
     <ul class="uk-width-1-6 uk-flex uk-flex-middle uk-flex-around uk-height-1-1 uk-margin-remove uk-padding-remove">
         <li><a href="https://www.instagram.com" uk-icon="instagram"></a></li>
