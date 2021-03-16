4032689ad2913a94ab496fe8db199b661368a419
NicoRiccitelli29 <nriccitelli@udesa.edu.ar>
2021-03-16 19:24:00 -0300

    Update App.js
    Puse al principio y final <react.fragments>. Corregi un h1 linea 11 que estaba rompiendo

--- primer-proyecto/src/App.js
+++ primer-proyecto/src/App.js
@@ -1,13 +1,14 @@
-
+import React from 'react';
 function App() {
   return (
-    <div className="App">
+<React.Fragment>
+  <div className="App">
     <header class="uk-background-primary">
       <section class=" uk-container uk-container-large">
         <div class="">
             <nav class="" uk-navbar>
                 <div class="uk-navbar-left">
-                    <h2 class="logo"><a href="">DNT App</a></h2>
+                    <h1>DNT App</h1>
                 </div>
               </nav>  
         </div>
@@ -15,19 +16,20 @@ function App() {
     </header> 
 
       <footer class="uk-position-bottom uk-position-fixed uk-background-primary uk-flex uk-flex-between uk-flex-middle">
-    <ul class="uk-width-1-6 uk-flex uk-flex-middle uk-flex-around uk-height-1-1 uk-margin-remove uk-padding-remove">
-        <li><a href="https://www.instagram.com" uk-icon="instagram"></a></li>
-        <li><a href="https://www.facebook.com" uk-icon="facebook"></a></li>
-        <li><a href="https://twitter.com" uk-icon="twitter"></a></li>
-    </ul>
-    <ul class="">
-        <li>Nicolás Riccitelli</li>
-        <li>Tomas Bongarra</li>
-        <li>Demián Lazarte</li>
-    </ul>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-</footer>
-    </div>
+       <ul class="uk-width-1-6 uk-flex uk-flex-middle uk-flex-around uk-height-1-1 uk-margin-remove uk-padding-remove">
+           <li><a href="https://www.instagram.com"> Instagram</a></li>
+            <li><a href="https://www.facebook.com"> Facebook</a></li>
+            <li><a href="https://twitter.com">Twitter</a></li>
+        </ul>
+        <ul class="">
+            <li>Nicolás Riccitelli</li>
+            <li>Tomas Bongarra</li>
+            <li>Demián Lazarte</li>
+        </ul>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      </footer>
+  </div>
+</React.Fragment>  
   );
 }
 
