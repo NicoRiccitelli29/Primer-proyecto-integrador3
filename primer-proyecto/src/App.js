2ff7d64b194eff826b13d44e45dbc8709b1f3cb4
Totobongarra <tomasbongarra00@gmail.com>
2021-03-17 18:31:27 -0300

    html y css
    hice el html y el css un poquito mas presentable. Estaria bueno poner bootstrap para poner cosas mas lindas, tipo las tarjetas o el header

--- primer-proyecto/src/App.js
+++ primer-proyecto/src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
 <React.Fragment>
   <div className="App">
-    <header class="uk-background-primary">
+    <header>
       <section class=" uk-container uk-container-large">
         <div class="">
             <nav class="" uk-navbar>
@@ -14,20 +14,68 @@ function App() {
         </div>
       </section> 
     </header> 
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
 
-      <footer class="uk-position-bottom uk-position-fixed uk-background-primary uk-flex uk-flex-between uk-flex-middle">
-       <ul class="uk-width-1-6 uk-flex uk-flex-middle uk-flex-around uk-height-1-1 uk-margin-remove uk-padding-remove">
+    <h1>Tarjetas</h1>
+
+    <main>
+      <div className='tarjeta'>
+        <h2>Foto:</h2>
+        <ul>
+          <li>Nombre:</li>
+          <li>Apellido:</li>
+          <li>Email:</li>
+          <li>Fecha de nacimiento (edad):</li>
+          
+        </ul>
+      </div>
+      <div className='tarjeta'>
+        <h2>Foto:</h2>
+        <ul>
+          <li>Nombre:</li>
+          <li>Apellido:</li>
+          <li>Email:</li>
+          <li>Fecha de nacimiento (edad):</li>
+          
+        </ul>
+      </div>
+      <div className='tarjeta'>
+        
+        <h2>Foto:</h2>
+        <ul>
+          <li>Nombre:</li>
+          <li>Apellido:</li>
+          <li>Email:</li>
+          <li>Fecha de nacimiento (edad):</li>
+          
+        </ul>
+        
+      </div>
+    </main>
+
+
+
+
+
+
+    
+
+    <footer >
+       <ul>
            <li><a href="https://www.instagram.com"> Instagram</a></li>
             <li><a href="https://www.facebook.com"> Facebook</a></li>
             <li><a href="https://twitter.com">Twitter</a></li>
         </ul>
-        <ul class="">
+        <ul>
             <li>Nicolás Riccitelli</li>
             <li>Tomas Bongarra</li>
             <li>Demián Lazarte</li>
         </ul>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-      </footer>
+    </footer>
   </div>
 </React.Fragment>  
   );
