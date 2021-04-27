4dab36606a645287c9a95f73456a3550554474bf
Totobongarra <tomasbongarra00@gmail.com>
2021-04-27 19:05:59 -0300

    hice filtro por edad

--- primer-proyecto/src/components/Main.js
+++ primer-proyecto/src/components/Main.js
@@ -57,20 +57,36 @@ class Main extends Component{
         let buscar = this.state.items.filter((busqueda)=>{
            let nombre = busqueda.name.first.toUpperCase()
            let apellido = busqueda.name.last.toUpperCase()
-           let edad = busqueda.dob.age.toString() 
-           return nombre.includes(filtroTarjetas) || apellido.includes(filtroTarjetas) || edad.includes(filtroTarjetas)
+           return nombre.includes(filtroTarjetas) || apellido.includes(filtroTarjetas) 
         })
         this.setState({
             items : buscar
         }
         )
 
-      //  console.log(this.state.nombreBuscado);
+      
+    }
+
+    filtrarPorEdad(edadBuscada){
+        let filtroTarjetas = document.querySelector(".filtro2").value.toUpperCase()
+        console.log(filtroTarjetas);
+        let buscar = this.state.items.filter((busqueda)=>{
+           let edad = busqueda.dob.age.toString() 
+           return edad.includes(filtroTarjetas)
+        })
+        this.setState({
+            items : buscar
+        }
+        )
+
+      
+    }
+
+    //  console.log(this.state.nombreBuscado);
         //let busquedaNombre = this.state.items.filter((item)=>{
           //  return item.name.first === nombreBuscado
         //})
         //this.setState({items: busquedaNombre})
-    }
 
     render(){
         return(
@@ -80,10 +96,13 @@ class Main extends Component{
             <button  onClick={this.agregarTarjetas.bind(this)} class="btn btn-outline-secondary" type="button" id="button-addon2">Buscar</button>
             </div>
             <div class="input-group mb-3">
-            <input onChange={this.filtrarTarjetas.bind(this)}  className="filtro" placeholder="Buscar..." aria-label="Nombre a buscar..." aria-describedby="button-addon3"></input>
-            <button  onClick={this.filtrarTarjetas.bind(this)} class="btn btn-outline-secondary" type="button" id="button-addon3">Buscar</button>
+            <input onChange={this.filtrarTarjetas.bind(this)}  className="filtro" placeholder="Nombre o apellido" aria-label="Nombre a buscar..." aria-describedby="button-addon3"></input>
+            <button  onClick={this.filtrarTarjetas.bind(this)} class="btn btn-outline-secondary" type="button" id="button-addon3">Filtrar</button>
             </div>
-           
+            <div class="input-group mb-3">
+            <input onChange={this.filtrarPorEdad.bind(this)}  className="filtro2" placeholder="Filtrar por edad" aria-label="Nombre a buscar..." aria-describedby="button-addon4"></input>
+            <button  onClick={this.filtrarPorEdad.bind(this)} class="btn btn-outline-secondary" type="button" id="button-addon4">Filtrar</button>
+            </div>
             <div>
                 <button className="botonReset" onClick={this.resetDefault.bind(this)}> <b>Reset</b></button>
             </div>
