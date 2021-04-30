d1052a6397bb65c24fbf11b6b9d8faabda7c5a5f
Demian <demianlazarte@gmail.com>
2021-04-30 16:47:10 -0300

    main

--- primer-proyecto/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-
-ReactDOM.render( <App /> , document.getElementById('root'));//Renderizo la vista (Lo mando a la vista)
-//Este render toma dos parametros, el primero es que vamos a enviar y el segundo es a donde lo envio. 
-
